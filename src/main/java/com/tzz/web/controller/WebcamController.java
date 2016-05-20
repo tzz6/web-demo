42585d9edf7c4f7ac4a0f0882436111ae553d0db
tzz6 <291232797@qq.com>
2016-05-20 23:11:39 +0800

    webcam拍照

--- src/main/java/com/tzz/web/controller/WebcamController.java
+++ src/main/java/com/tzz/web/controller/WebcamController.java
@@ -4,13 +4,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/webcam")
 public class WebcamController extends BaseController {
 
+	/** 拍照 */
+	@RequestMapping(value = "/photograph", method = RequestMethod.GET)
+	public String index(@RequestParam(value = "id", required = false) String id, HttpServletRequest request,
+			ModelMap model) {
+		System.out.println("-------------------------------id:" + id);
+		return "/webcam/photograph";
+	}
+	
 	@RequestMapping(value = "/uploadImage", method = RequestMethod.POST)
 	public void saveFile(HttpServletRequest request, HttpServletResponse response) {
 //		String w = request.getParameter("w");
